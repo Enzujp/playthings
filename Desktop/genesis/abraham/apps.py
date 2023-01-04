8812e1ac4c13faf9c7542aec3e76fad04189b701
Enzu <100221308+Enzujp@users.noreply.github.com>
2023-01-04 12:34:43 +0100

    Delete Desktop/genesis directory
    no longer required

--- Desktop/genesis/abraham/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AbrahamConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abraham'
--- Desktop/genesis/abraham/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse
-
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Welcome, Abraham!")
